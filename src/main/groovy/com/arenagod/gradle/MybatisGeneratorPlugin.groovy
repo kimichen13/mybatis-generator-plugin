4e46934ca62e832def1a4af3c88d1db244e2b40e
chichiritsuka <chichiri0126@gmail.com>
2017-05-09 21:17:58 +0900

    Update MybatisGeneratorPlugin.groovy

--- src/main/groovy/com/arenagod/gradle/MybatisGeneratorPlugin.groovy
+++ src/main/groovy/com/arenagod/gradle/MybatisGeneratorPlugin.groovy
@@ -22,7 +22,7 @@ class MybatisGeneratorPlugin implements Plugin<ProjectInternal> {
                 def config = project.configurations.getAt('mybatisGenerator')
                 if (config.dependencies.empty) {
                     project.dependencies {
-                        mybatisGenerator 'org.mybatis.generator:mybatis-generator-core:1.3.2'
+                        mybatisGenerator 'org.mybatis.generator:mybatis-generator-core:1.3.5'
                         mybatisGenerator 'mysql:mysql-connector-java:5.1.36'
                         mybatisGenerator 'tk.mybatis:mapper:3.3.2'
                     }

2823175287eb9fb757e3778147b62c8bb603c0bc
Kimi Chen <kimichen13@gmail.com>
2019-07-28 01:43:57 +0300

    Test successful with generate table related.

---
+++ src/test/groovy/com/thinkimi/gradle/dao/CustomerMapper.java
@@ -0,0 +1,30 @@
+package com.thinkimi.gradle.dao;
+
+import com.thinkimi.gradle.model.Customer;
+import com.thinkimi.gradle.model.CustomerExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface CustomerMapper {
+    long countByExample(CustomerExample example);
+
+    int deleteByExample(CustomerExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Customer record);
+
+    int insertSelective(Customer record);
+
+    List<Customer> selectByExample(CustomerExample example);
+
+    Customer selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") Customer record, @Param("example") CustomerExample example);
+
+    int updateByExample(@Param("record") Customer record, @Param("example") CustomerExample example);
+
+    int updateByPrimaryKeySelective(Customer record);
+
+    int updateByPrimaryKey(Customer record);
+}
